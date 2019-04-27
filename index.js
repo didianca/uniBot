c18aab0c3ec0d3bbd33368aeaadf0d3a247d0602
didianca <cazaranka@gmail.com>
2019-04-27 01:10:50 -0400

    implement ping pong connect youtube API implement youtube search linking youtube vids to channel error handling

--- index.js
+++ index.js
@@ -1,38 +1,82 @@
 const Discord = require('discord.js');
-const{prefix,token, giphyToken} = require('./config');
+const {prefix, token, giphyToken, ytToken} = require('./config');
 const client = new Discord.Client();
-const quotes =require('./quotes');
+const quotes = require('./quotes');
 const GphApiClient = require('giphy-js-sdk-core');
 giphy = GphApiClient(giphyToken);
+const fetch = require('node-fetch');
 
-client.once('ready',()=>{
-    console.log("Ready!")
-});
-client.on('message',message => {
-    if(message.content.startsWith(`${prefix}uni`)){
+client.once('ready', () => {console.log("Unicorn trotting!")});
+
+//!uni
+client.on('message', message => {
+    if (message.content.startsWith(`${prefix}uni`)) {
         let member = message.mentions.members.first() || "";
-        giphy.search('gifs',{"q":["magical","unicorn","animated candies","fairyland"]})
-            .then((response)=>{
+        giphy.search('gifs', {"q": ["magical", "unicorn", "animated candies", "fairyland"]})
+            .then((response) => {
                 //getting one gif
                 let totalResponses = response.data.length;
-                let responseIndex=Math.floor((Math.random() *10)+1) % totalResponses;
+                let responseIndex = Math.floor((Math.random() * 10) + 1) % totalResponses;
                 let finalResponse = response.data[responseIndex];
-
                 //getting one quote
                 let totalQuotes = quotes.length;
-                let quotesIndex = Math.floor((Math.random() *10)+1) % totalQuotes;
+                let quotesIndex = Math.floor((Math.random() * 10) + 1) % totalQuotes;
                 let quote = quotes[quotesIndex];
-                message.channel.send(`${quote} ${member} :unicorn:`,{
-                    files:[finalResponse.images.fixed_height.url]
+
+                message.channel.send(`${quote} ${member} :unicorn:`, {
+                    files: [finalResponse.images.fixed_height.url]
                 });
             })
-            .catch((e)=>{
-                message.channel.send('Nasty error')
-        });
-
+            .catch(() => {
+                message.channel.send('Hiccup...')
+            });
     }
 });
 
+//ping-pong
+client.on('message', msg => {
+    if (msg.content === 'ping') msg.reply('pong');
+});
+
+//send youtube videos
+client.on('message', async msg => {
+    if (msg.content.startsWith(`${prefix}yt`)) {
+        const content = msg.content;
+        const vidName = content.indexOf('!yt') + 1;
+        const result = content.substring(vidName + 1);
+        msg.channel.send(await getVidLink(result));
+    }
+});
+
+const getVidLink = async (vidName) => {
+    try {
+        const encodedVidName = encodeURI(vidName);
+        console.log(encodedVidName);
+        const params = {url: `https://www.googleapis.com/youtube/v3/search?part=id&maxResults=2&order=viewCount&type=video&key=${ytToken}&q=${encodedVidName}`};
+        const response = await request('GET', params);
+        const videoId = response.items[0].id.videoId;
+        return `https://www.youtube.com/watch?v=${videoId}`;
+    } catch (e) {
+        return "This video doesn't exist..."
+    }
+};
+
+const request = async (verb, params) => {
+    let res;
+    res = verb.toUpperCase() === 'GET' ?
+        await fetch(
+            params.url, {
+                method: verb,
+                headers: params.headers
+            }) :
+        await fetch(
+            params.url, {
+                method: verb,
+                body: params.body,
+                headers: params.headers
+            });
+    return await res.json();
+};
 
 client.login(token);
 
