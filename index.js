0bd876e2363fc61abdfa31b46c84e35ebcc162fe
didianca <didi@neels.io>
2020-06-26 11:36:10 -0400

    created utils folder and moved util.js in it; created scrim-utils and moved all functs other than scrim there; fixed disparity issue where it would show NaN; removed commented out functions from index that i already  set up in a different file

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const { unicorn } = require('./services/unicorn');
 const { youTube } = require('./services/youtube');
 const { setInGameName, scrim, } = require('./services/valorant'); // todo wip
 const { test } = require('./test'); //todo wip
-const util = require('./util');
+const {formatJson, request} = require('./utils/util');
 const {
     UNICORN_PREFIX,
     PING_PONG_PREFIX,
@@ -69,74 +69,68 @@ client.on('message', async message => {
         case(content.startsWith(VALORANT_SCRIM_PREFIX)):
             await scrim(message);
             break;
+        case(content.startsWith(VALORANT_DONE_PREFIX)):
+            channel.send('Done command case works.')
+            break;
         case(content.startsWith(VALORANT_LEADERBOARD_PREFIX)):
             channel.send('Lead board command case works.');
             break;
         case(content.startsWith(VALORANT_ELO_PREFIX)):
             channel.send('Elo command case works.');
             break;
-        case(content.startsWith(VALORANT_DONE_PREFIX)):
-            channel.send('Done command case works.')
-            break;
     }
 })
 
-// UNICORN - !uni
-// client.on('message', async message => {
-//     const {content, author, reply } = message;
-//     try {
-//         if (content.startsWith('!scrim') || content.startsWith('!s')) try {
-//             await scrim(message);
-//         } catch (e) {
-//             console.log(e)
-//         }
-//         else if (content.startsWith('!done') || content === '!d') try {
-//             await addMatch(message);
-//         } catch (e) {
-//             console.log(e)
-//         }
-//         else if (content.startsWith('!elo')) try {
-//             await getElo(message);
-//         } catch (e) {
-//             console.log(e)
-//         }
-//         else if (content.startsWith('!helpme')) try {
-//             reply(helpMsg)
-//         } catch (e) {
-//             console.log(e)
-//         }
-//         else if (content.startsWith('!leaderboard')) try {
-//             const res = await db.getLeaderboard(); // todo create method in db
-//             const leaderboard = util.formatJson(res)
-//                 .split('name').join('\n')
-//                 .split('averageScore').join('')
-//                 .split(':').join('')
-//                 .split('[').join('')
-//                 .split(']').join('');
-//
-//             reply(leaderboard);
-//         } catch (e) {
-//             console.log(e)
-//         }
-//     } catch {
-//         reply('Hiccup...');
-//     }
-// });
+client.on('message', async message => {
+    const {content, author, reply } = message;
+    try {
+        if (content.startsWith('!done') || content === '!d') try {
+            await addMatch(message);
+        } catch (e) {
+            console.log(e)
+        }
+        else if (content.startsWith('!elo')) try {
+            await getElo(message);
+        } catch (e) {
+            console.log(e)
+        }
+        else if (content.startsWith('!helpme')) try {
+            reply(helpMsg)
+        } catch (e) {
+            console.log(e)
+        }
+        else if (content.startsWith('!leaderboard')) try {
+            const res = await db.getLeaderboard(); // todo create method in db
+            const leaderboard = formatJson(res)
+                .split('name').join('\n')
+                .split('averageScore').join('')
+                .split(':').join('')
+                .split('[').join('')
+                .split(']').join('');
+
+            reply(leaderboard);
+        } catch (e) {
+            console.log(e)
+        }
+    } catch {
+        reply('Hiccup...');
+    }
+});
 
 // const addMatch = async (message) => {
 //     let response;
 //     for (let [sf, attachment] of message.attachments) {
 //         console.log(attachment);
-//         response = await util.request('POST', {
+//         response = await request('POST', {
 //             url: 'https://ft9v591wbd.execute-api.us-east-1.amazonaws.com/dev/ocr', // ??????
 //             body: JSON.stringify(attachment)
 //         });
 //
-//         const cleanMessage = util.formatJson(response);
+//         const cleanMessage = formatJson(response);
 //         const oldScores = await db.getEveryonesAverageScore(response); // todo create method
 //         const newScores = await db.recordCombatScores(response); // todo create method
 //         const averageEloChange = getScoreAverageChangeMsg(newScores, oldScores);
-//         const averageEloChangeCleanMsg = util.formatJson(averageEloChange);
+//         const averageEloChangeCleanMsg = formatJson(averageEloChange);
 //         console.log(averageEloChangeCleanMsg);
 //         message.channel.send('Recorded:\n' + cleanMessage);
 //         message.channel.send('\n\nElo change:\n' + averageEloChangeCleanMsg + "\n If you don't see your name here you need to add your in game name (!setign {your ign})")
@@ -155,47 +149,6 @@ client.on('message', async message => {
 //     const elo = await db.getAverageScore(message.author.id); // todo create method
 //     console.log(elo);
 //     message.reply('Average Elo: ' + elo.averageScore); // todo create method
-// };
-
-// const splitTeams = (aggregateUsers) => {
-//     let split = false;
-//     const team0 = [];
-//     const team1 = [];
-//     for (let i = 0; i < aggregateUsers.length; i++) {
-//         if (i === Math.floor(aggregateUsers.length / 2)) split = true;
-//         if (split) team0.push(aggregateUsers[i]);
-//         else team1.push(aggregateUsers[i]);
-//     }
-//     return [team0, team1];
-// };
-
-// const  determineDisparity = async (teamsArr) => {
-//     // console.log('TEAM 0', teamsArr[0]);
-//     // console.log('TEAM 1', teamsArr[1]);
-//     const team0Elo = await totalTeamElo(teamsArr[0]);
-//     const team1Elo = await totalTeamElo(teamsArr[1]);
-//     console.log(team1Elo);
-//     const team0AvgElo = team0Elo / teamsArr[0].length;
-//     const team1AvgElo = team1Elo / teamsArr[1].length;
-//     return [team0AvgElo, team1AvgElo];
-// };
-
-// const totalTeamElo = async (teamArr) => {
-//     let totalElo = 0;
-//     const averages = await Promise.all(teamArr.map(player => {
-//         const average = db.getAverageScore(player.id);
-//         return average ? average : 125; //if the player is new give them a 125 cmbt score
-//     }));
-//     averages.forEach(player => totalElo += player.averageScore);
-//     return totalElo;
-// };
-
-// const shuffle = (array) => {
-//     for (let i = array.length - 1; i > 0; i--) {
-//         const j = Math.floor(Math.random() * (i + 1));
-//         [array[i], array[j]] = [array[j], array[i]];
-//     }
-//     return array;
 // };
 
 // const request = async (verb, params) => {
--- services/valorant.js
+++ services/valorant.js
@@ -6,9 +6,14 @@ const {
     insertPlayer,
     getAllPlayers,
     updatePlayerName,
-    getAverageScoreFromMatches,
     getPlayersInVoiceChannel,
 } = require('../db');
+const {
+    shuffle,
+    splitTeams,
+    totalTeamElo,
+    determineDisparity,
+} = require('../utils/scrim-utils');
 const { db } = require('../db');
 
 const setInGameName = async (playerId, name) => {
@@ -32,7 +37,6 @@ const scrim = async (message, disparityDifferenceThreshold = 1) => {
         usersInChannel.push(name);
         memberIds.push(member.id);
         const playerExists = await  getPlayerById(member.id)
-        console.log(name, '>>>>>>>>>>>>>>>>>>>>>>>>> playerExists: ', playerExists)
         if (!playerExists)  await insertPlayer(member.id, name);
         if(playerExists && playerExists.name !== name) await updatePlayerName(member.id, name);
     }
@@ -44,64 +48,24 @@ const scrim = async (message, disparityDifferenceThreshold = 1) => {
     const playersData = await getPlayersInVoiceChannel(memberIds);
     const shuffledPlayers = shuffle(playersData);
     const teams = splitTeams(shuffledPlayers);
-    const disparityArr = await determineDisparity(teams);
-    const disparityDifference = disparityArr[0] - disparityArr[1];
+    const disparity = await determineDisparity(teams);
     let teamMessage = '';
-    if (Math.abs(disparityDifference) >= disparityDifferenceThreshold) {
+    if (Math.abs(disparity) >= disparityDifferenceThreshold) {
         if (shuffleCounter % 1 === 0) {
             message.reply('unable to balance threshold incremented to:', disparityDifferenceThreshold += 0.5);
             return scrim(message, disparityDifferenceThreshold += 0.5);
         }
         return scrim(message, disparityDifferenceThreshold);
     }
-    // await db.deleteInProgressMatch(); //there can only be 1 in progress match
-    // await db.addInProgressMatch(teams);
     teams.forEach(team => {
         teamMessage += `\n> Team ${team[0].name}:\n`;
         team.forEach(user => {
             teamMessage += `${user.name}\n`;
         });
     });
-    teamMessage += `\n First team favored by ${disparityDifference}. Threshold: ${disparityDifferenceThreshold}`;
+
+    teamMessage += `\n First team favored by ${disparity}. Threshold: ${disparityDifferenceThreshold}`;
     message.channel.send(teamMessage);
-};
-
-const shuffle = (array) => {
-    for (let i = array.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [array[i], array[j]] = [array[j], array[i]];
-    }
-    return array;
-};
-
-const splitTeams = (players) => {
-    let split = false;
-    const firstTeam = [];
-    const secondTeam = [];
-    for (let i = 0; i < players.length; i++) {
-        if (i === Math.floor(players.length / 2)) split = true;
-        if (split) firstTeam.push(players[i]);
-        else secondTeam.push(players[i]);
-    }
-    return [firstTeam, secondTeam];
-};
-
-const totalTeamElo = async (teamArr) => {
-    let totalElo = 0;
-    const averages = await Promise.all(teamArr.map(player => {
-        const average = getAverageScoreFromMatches(player.id);
-        return average ? average : 100; //if the player is new give them a 100 cmbt score
-    }));
-    averages.forEach(player => totalElo += player.elo);
-    return totalElo;
-};
-
-const  determineDisparity = async (teamsArr) => {
-    const firstTeamElo = await totalTeamElo(teamsArr[0]);
-    const secondTeamElo = await totalTeamElo(teamsArr[1]);
-    const firstTeamAverageElo = firstTeamElo / teamsArr[0].length;
-    const secondTeamAverageElo = secondTeamElo / teamsArr[1].length;
-    return firstTeamAverageElo - secondTeamAverageElo;
 };
 
 module.exports = {
